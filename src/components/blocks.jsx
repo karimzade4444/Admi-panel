628379daf58cd0d8f16031d5393e3cd5d1f0cd85
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 02:15:42 +0500

    blockstop

--- src/components/blocks.jsx
+++ src/components/blocks.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { getUsers } from "../api/users";
+import { DeleteOutlined, EditOutlined, EyeOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button,  } from "antd";
 
 
 const Blocks = () => {
@@ -8,14 +10,27 @@ const Blocks = () => {
     const dispatch = useDispatch();
     useEffect(()=>{dispatch(getUsers(search))},[search])
   return (
-    <div>
-        {data?.map((el)=>(
-            <div key={el.id} className="w-150 h-100">
-              
+    <div className=" grid grid-cols-3 place-items-center gap-5">
+      {data?.map((el) => (
+        <div key={el.id} className="w-100 h-70 bg-white">
+          <div className="flex justify-between items-start">
+            <h1>{el.name}</h1>
+            <div className=" flex justify-center items-center gap-3">
+              <Button type="primary" shape="circle">
+                <EyeOutlined />
+              </Button>
+              <Button type="primary" shape="circle">
+                <EditOutlined />
+              </Button>
+              <Button type="primary" shape="circle" danger>
+                <DeleteOutlined />
+              </Button>
             </div>
-        ))}
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Blocks
