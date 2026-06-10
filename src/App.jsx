fccb49dd9e3f2ec81de4e128503cc53f6ca12f9a
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 01:36:41 +0500

    middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Top from './components/top'
+import Middle from './components/middle'
 
 const App = () => {
   return (
     <div>
       <Top/>
+      <Middle/>
      
       
     </div>
---
+++ src/components/middle.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Flex, Input, Button } from "antd";
+import { Select, Space } from "antd";
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+
+const Middle = () => {
+  return (
+    <div className="mt-10">
+      <div className="w-[95%] h-20 bg-white m-auto rounded-xl border border-gray-300 flex justify-between items-center p-5">
+        <Input placeholder="Поиск" variant="filled" className="w-100! "></Input>
+        <div className=" flex justify-center items-center gap-5">
+          <Select
+            defaultValue="disabled"
+            style={{ width: 120 }}
+            onChange={handleChange}
+            options={[
+              { value: "Работа", label: "Работа" },
+              { value: "Личное", label: "Личное" },
+              { value: "Проект", label: "Проект" },
+              { value: "disabled", label: "Disabled", disabled: true },
+            ]}
+          />
+          <Button type="primary">+ Добавить</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Middle;
