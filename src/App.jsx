8838c4a81e9f4a9fe827aca56be0a5fe31e7afce
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 01:54:10 +0500

    users

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+
 import Top from './components/top'
 import Middle from './components/middle'
 import Bottom from './components/bottom'
+"react-redux";
+
 
 const App = () => {
+
   return (
     <div>
       <Top/>
--- src/components/blocks.jsx
+++ src/components/blocks.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { getUsers } from "../api/users";
 
 
 const Blocks = () => {
+    const { data, search } = useSelector((store) => store.marketPlace);
+    const dispatch = useDispatch();
+    useEffect(()=>{dispatch(getUsers(search))},[search])
   return (
-    <div>blocks</div>
+    <div>
+        {data?.map((el)=>(
+            <div key={el.id} className="w-150 h-100">
+              
+            </div>
+        ))}
+    </div>
   )
 }
 
