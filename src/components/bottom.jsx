992e09be8f1c21e96bc12b8f455f874d877c7104
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 01:41:37 +0500

    blocks

--- src/components/bottom.jsx
+++ src/components/bottom.jsx
@@ -1,9 +1,11 @@
-
+import Blocks from "./blocks";
 
 const Bottom = () => {
   return (
     <div className="mt-10">
-      <div className="w-[95%]  m-auto"></div>
+      <div className="w-[90%]  m-auto">
+        <Blocks/>
+      </div>
     </div>
   );
 }
