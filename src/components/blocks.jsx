6fb9ad7fd17d5ea137fdb915ec05a068d2c924fa
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-11 02:36:38 +0500

    category

--- src/components/blocks.jsx
+++ src/components/blocks.jsx
@@ -9,10 +9,17 @@ const Blocks = () => {
     const { data, search } = useSelector((store) => store.marketPlace);
     const dispatch = useDispatch();
     useEffect(()=>{dispatch(getUsers(search))},[search])
+    const categoryColors = {
+      "Работа": "bg-blue-500/50 text-blue", 
+      "Проект": "bg-purple-500/50 text-purple",
+      "Личное": "bg-green-500/50 text-green", 
+    };
   return (
     <div className=" grid grid-cols-3 place-items-center gap-5">
-      {data?.map((el) => (
-        <div key={el.id} className="w-100 h-70 bg-white rounded-xl border border-gray-300 p-5">
+      {data?.map((el) => {
+        const bgClass =categoryColors[el.category]|| "bg-white text-black";
+        return(
+        <div key={el.id} className={`w-100 h-60 bg-white rounded-xl border border-gray-300 p-5 ${bgClass}`}>
           <div className="flex justify-between items-start h-15">
             <h1 className=" font-semibold text-2xl">{el.name}</h1>
             <div className=" flex justify-center items-center gap-3">
@@ -27,10 +34,10 @@ const Blocks = () => {
               </Button>
             </div>
           </div>
-          <div className="h-20"><p className=" text-gray-500">{el.title}</p></div>
-          
+          <div className="h-25"><p className=" text-gray-500">{el.title}</p></div>
+          <div><p>{el.category}</p></div>
         </div>
-      ))}
+)})}
     </div>
   );
 }
